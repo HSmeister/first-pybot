0ba2a353471cefbc604152d191c73bbeed812ee2
HSmeister <germannene15@gmail.com>
2020-11-21 17:23:37 +0100

    modified bot and added city

--- telebot.py
+++ telebot.py
@@ -1,5 +1,6 @@
 import os
-from telethon import TelegramClient, events
+import telethon
+from telethon import TelegramClient, events, Button
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,22 +9,47 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 API_ID = os.getenv('API_ID')
 API_HASH = os.getenv('API_HASH')
 
+
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+file = open('city.txt')
+
 
 @bot.on(events.NewMessage(pattern='/start'))
 async def start(event):
     """Send a message when the command /start is issued."""
-    await event.respond('Привет! Я бот-попугай, повторяю твои сообщения)')
+    await event.respond('Привет! Я знаю наизусть все населённые пункты России. Введи /play, чтобы сыграть со мной в города😏')
     raise events.StopPropagation
 
-@bot.on(events.NewMessage)
-async def echo(event):
-    """Echo the user message."""
-    await event.respond(event.text)
+
+@bot.on(events.NewMessage(pattern='/play'))
+async def start(event):
+        await event.respond('Начнём! Ты первый😋')
+        lis = list()
+        @bot.on(events.NewMessage)
+        async def game(event):
+            message = event.text
+            if message in file:
+                if not message in lis:
+                    let = message[len(message)-1]
+                    await event.respond(getword(let))
+            else:
+                await event.respond('Насколько я знаю, такого населённого пункта не существует в России. Попробуй еще раз😉')
+
+
+def getword(letter):
+    for line in file:
+        if line[0] == letter:
+            if line not in lis:
+                return line
+                break
+
+
+
 
 def main():
     """Start the bot."""
     bot.run_until_disconnected()
+
 
 if __name__ == '__main__':
     main()
